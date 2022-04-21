ecbf9b2c726c7636cddd4f3937674be17afd2ded
Artem <68487549+gorodkoff44@users.noreply.github.com>
2022-04-21 13:56:29 +0300

    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,27 +21,28 @@ namespace Страховая
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer timer = new DispatcherTimer();
+        private readonly DispatcherTimer timer = new DispatcherTimer();
         public static DateTime Today { get; }
         public MainWindow()
         {
             InitializeComponent();
             //время
-            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Interval = TimeSpan.FromSeconds(15);
+
             timer.Tick += Timer_Tick;
             timer.Start();
             DateTime thisDay = DateTime.Today;
-            lblDate.Content = thisDay.ToString("D");
+            lblDate.Content = thisDay.ToString("D"); //Вывод даты
         }
         void Timer_Tick(object sender, EventArgs e) //время
         {
             string time = DateTime.Now.ToLongTimeString(); ; //хранит полное время
             string[] words = time.Split(new char[] { ':' }); //разделение данных часов минут и секунд
-            string lbl = words[0] + ":" + words[1]; //сборка часов и минут в 1 переменную
-            lblTime.Content = lbl; //вывод
+            lblTime.Content = words[0] + ":" + words[1]; //сборка часов и минут в 1 переменную и вывод
+            //Console.WriteLine("Timer_Tick");
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e) //Объекты страхования
         {
             object_str Object_Str = new object_str();
             Object_Str.ShowDialog();
@@ -49,21 +50,21 @@ namespace Страховая
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e) //Договора
         {
             dogovora Dogovora = new dogovora();
             Dogovora.ShowDialog();
             Dogovora.Owner = this;
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void Button_Click_2(object sender, RoutedEventArgs e) //Страховые выплаты
         {
             StrWip strWip = new StrWip();
             strWip.ShowDialog();
             strWip.Owner = this;
         }
 
-        private void Button_Click_3(object sender, RoutedEventArgs e)
+        private void Button_Click_3(object sender, RoutedEventArgs e) //Выход
         {
             Close();
         }
